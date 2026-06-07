2449d4cec477eb931a0338c7c10491e8d3265eb9
Bhanu <priyabhanubp376@gmail.com>
2026-06-07 15:54:32 +0530

    fix: Job model import case issue

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Job = require("../models/Job");
+const Job = require("../models/job");
 
 // ADD JOB
 router.post("/", async (req, res) => {
